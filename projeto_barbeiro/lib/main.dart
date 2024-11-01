51b4ef347b201ac962a3cad36b3a4fbb16293ed6
GregoryVinicius <gregoryviniciuscla@gmail.com>
2024-10-31 22:31:05 -0300

    Com problema na lista de agendamento

--- projeto_barbeiro/lib/main.dart
+++ projeto_barbeiro/lib/main.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:projeto_barbeiro/meu_app.dart';
+import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+import 'package:sqflite/sqflite.dart';
 
 main() {
+  sqfliteFfiInit();
+  databaseFactory = databaseFactoryFfi;
   runApp(const MeuApp());
 }
